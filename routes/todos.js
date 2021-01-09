f1d3066f48686b1346039524e7acec7c22762f54
Dmitry Koka <koka.d.wezom@gmail.com>
2021-01-09 23:35:13 +0200

    Added delete method

--- routes/todos.js
+++ routes/todos.js
@@ -29,12 +29,18 @@ router.post('/create', async (req, res) => {
 })
 
 router.post('/complete', async (req, res) => {
-    const todo = await Todo.findById(req.body.id)
+    const todo = await Todo.findById(req.body.id);
 
     todo.completed = !!req.body.completed
     await todo.save()
 
     res.redirect('/')
 })
+
+router.get('/delete/:id', async (req, res) => {
+    await Todo.findByIdAndDelete(req.params.id);
+    res.redirect('/')
+})
+
 
 module.exports = router
